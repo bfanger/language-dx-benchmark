151798a81320a21916341182d01f0c31290545f9
Bob Fanger <bfanger@gmail.com>
2024-04-26 16:09:05 +0200

    chore: Updated versions

--- docker_run.sh
+++ docker_run.sh
@@ -3,17 +3,17 @@ set -e
 TIMEFORMAT=" in %Rs"
 
 # Compiled languages
-docker run --rm -it -v $PWD/go:/mnt golang bash -e /mnt/run.sh
-docker run --rm -it -v $PWD/swift:/mnt swift bash -e /mnt/run.sh
-docker run --rm -it -v $PWD/csharp:/mnt mcr.microsoft.com/dotnet/sdk bash -e /mnt/run.sh
-docker run --rm -it -v $PWD/rust:/mnt rust bash -e /mnt/run.sh
-docker run --rm -it -v $PWD/typescript:/mnt node bash -e /mnt/run.sh
-docker run --rm -it -v $PWD/typescript_deno:/mnt denoland/deno bash -e /mnt/run.sh
-docker run --rm -it -v $PWD/typescript_bun:/mnt oven/bun bash -e /mnt/run.sh
+docker run --rm -it -v $PWD/go:/mnt golang:latest bash -e /mnt/run.sh
+docker run --rm -it -v $PWD/swift:/mnt swift:latest bash -e /mnt/run.sh
+docker run --rm -it -v $PWD/csharp:/mnt mcr.microsoft.com/dotnet/sdk:latest bash -e /mnt/run.sh
+docker run --rm -it -v $PWD/rust:/mnt rust:latest bash -e /mnt/run.sh
+docker run --rm -it -v $PWD/typescript:/mnt node:latest bash -e /mnt/run.sh
+docker run --rm -it -v $PWD/typescript_deno:/mnt denoland/deno:latest bash -e /mnt/run.sh
+docker run --rm -it -v $PWD/typescript_bun:/mnt oven/bun:latest bash -e /mnt/run.sh
 
 
 # Interpreted languages 
-docker run --rm -it -v $PWD/php:/mnt php bash -e /mnt/run.sh
-docker run --rm -it -v $PWD/python:/mnt python bash -e /mnt/run.sh
-docker run --rm -it -v $PWD/javascript:/mnt node bash -e /mnt/run.sh
-docker run --rm -it -v $PWD/typescript_esbuild:/mnt node bash -e /mnt/run.sh
+docker run --rm -it -v $PWD/php:/mnt php:latest bash -e /mnt/run.sh
+docker run --rm -it -v $PWD/python:/mnt python:latest bash -e /mnt/run.sh
+docker run --rm -it -v $PWD/javascript:/mnt node:latest bash -e /mnt/run.sh
+docker run --rm -it -v $PWD/typescript_esbuild:/mnt node:latest bash -e /mnt/run.sh
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,6 @@
-
+use serde_json::Value;
 
 fn main() {
-    let data = json::parse(r#"{ "a": 1, "b": 2 }"#).unwrap();
-    println!("{}", data["a"].as_f32().unwrap() + data["b"].as_f32().unwrap())
+    let data: Value = serde_json::from_str(r#"{ "a": 1, "b": 2 }"#).unwrap();
+    println!("{}", data["a"].as_f64().unwrap() + data["b"].as_f64().unwrap())
 }
